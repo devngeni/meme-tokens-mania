0a0608a9b26dc31f9629f5aaf0b5ef3b393c4edd
Dennis-Codes <dennis@ngeni.io>
2023-08-09 21:30:54 +0300

    converted characters to html code

--- src/sections/LandingSection.tsx
+++ src/sections/LandingSection.tsx
@@ -17,7 +17,7 @@ export default function LandingSection() {
             Prepare for the most epic battle of the century
           </LandingTitle>
           <LandingSubtitle>
-            Witness Elon "Space-King" Musk duke it out with Mark "Lizard Lord"
+            Witness Elon &#x275D;Space-King&#x275E; Musk duke it out with Mark &#x275D;Lizard Lord&#x275E;
             Zuckerberg in the Web3 coliseum!, BSC is hosting the ultimate
             meme coin showdown: WWElon. Grab your seat in this crypto clash now!
             🐍
